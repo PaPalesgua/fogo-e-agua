3693d46e0863e65b91ff160e12c37f8e8f9b8d62
PaPalesgua <101650831+PaPalesgua@users.noreply.github.com>
2022-09-20 10:29:29 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var box
+
+function preload(){
+ 
+}
+
+function setup() {
+  createCanvas(400, 400);
+box = createSprite(200,200,40,40)
+
+
+}
+
+function draw() {
+  background(180);
+  
+  drawSprites();
+}
+
